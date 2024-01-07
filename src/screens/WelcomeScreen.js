8e4642b3b88fac12af2cf3a3031144164bcccb5f
Olayemi <ogunrinde_olayemi@yahoo.com>
2024-01-07 10:23:12 +0100

    setLoading

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { commonStyles } from '../styles/commonStyles';
 import { welcomeStyles } from '../styles/welcomeStyles';
 
 const WelcomeScreen = ({ navigation }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigateToSignUp = () => {
+    setIsLoading(true); 
+
+    setTimeout(() => {
+      setIsLoading(false); 
+      navigation.navigate('SignUp'); 
+    }, 2000); 
+  };
+
   return (
     <View style={commonStyles.container}>
       <Text style={welcomeStyles.title}>Welcome!</Text>
-      <TouchableOpacity style={welcomeStyles.button} onPress={() => navigation.navigate('SignUp')}>
-        <Text style={welcomeStyles.buttonText}>Get Started</Text>
+      <TouchableOpacity
+        style={welcomeStyles.button}
+        onPress={isLoading ? null : navigateToSignUp} 
+      >
+        {isLoading ? ( 
+          <ActivityIndicator size="small" color="white" />
+        ) : (
+          <Text style={welcomeStyles.buttonText}>Get Started</Text>
+        )}
       </TouchableOpacity>
     </View>
   );
